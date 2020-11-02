4c35ee6715bf9ff561bd0e7f53e17a2bc8d2e489
nduitz <nick.duitz@9elements.com>
2020-11-02 12:08:27 +0100

    welect demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,87 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(WebViewExample());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class WebViewExample extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  WebViewExampleState createState() => WebViewExampleState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class WebViewExampleState extends State<WebViewExample> {
+  WebViewController _controller;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return WebView(
+      initialUrl: "about:blank",
+      javascriptMode: JavascriptMode.unrestricted,
+      onWebViewCreated: (WebViewController webViewController) {
+        _controller = webViewController;
+        _loadWelect(context);
+      },
+      onPageStarted: (Void) {
+        injectWelectMessageListener(_controller, context);
+      },
+      onPageFinished: (Void) {
+        injectWelectMessageListener(_controller, context);
+      },
+      javascriptChannels: <JavascriptChannel>[
+        _welectJavascriptChannel(context),
+      ].toSet(),
     );
+  }
+
+  JavascriptChannel _welectJavascriptChannel(BuildContext context) {
+    // register a JavascriptChannel so we can receive messages from the welect process
+    return JavascriptChannel(
+        name: 'Welect',
+        onMessageReceived: (JavascriptMessage message) {
+          var eventData = jsonDecode(message.message);
+          switch (eventData['type']) {
+            case "welect:payload":
+              print('process finished, token: ${eventData['payload']}');
+              break;
+            case "cancel":
+              print("an error occured, process could not be finished");
+              break;
+            default:
+          }
+        });
+  }
+
+  _loadWelect(BuildContext context) async {
+    const externalId = "TjB6rd0Q9hU4Na4jQa9bFGWaV85e";
+    // see if welect is available by doing a preflight check.
+    // response looks like this:
+    // {available: true | false, entry_url: "url_to_welect"}
+    // body can be empty or contain the gdpr object given by the tcf-api (gdpr_consent: tcf-object)
+    var response = await http
+        .post('https://www.welect.de/api/v2/preflight/${externalId}', body: {});
+    var responseJSON = jsonDecode(response.body);
+    // TODO: proper error handling
+    if (responseJSON['available']) {
+      _controller.loadUrl(responseJSON['entry_url']);
+    }
+  }
+
+  void injectWelectMessageListener(
+      WebViewController controller, BuildContext context) async {
+    // apparently the webview plugin is not able to directly access the messages posted to window.
+    // thus we have to add the event listener manually and forward events to the registered JavascriptChannel
+    await controller.evaluateJavascript(
+        "window.addEventListener('message', (event) => { Welect.postMessage(JSON.stringify(event.data)); }, true);");
   }
 }
